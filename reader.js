d7f2a9a414adb4ed5705d5de17fe13a6d66ced5f
ArnoldDadayan <dadayanaa@yahoo.com>
2021-03-24 18:18:39 +0400

    Added reader.js file

---
+++ reader.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+const findTargetStringFromSource = (str) => {
+  fs.readFile('./notes.txt', 'utf8' , (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(data)
+  
+    const content = data.split("\n")
+    let maxValue = 0;
+    let target = -1;
+    content.forEach((row, index) => {
+      if(row.match(str)) {
+        if(row.match(str).index > maxValue) {
+          maxValue = row.match(str).index;
+          target =  row.match(str).input;
+        }
+      }
+    })
+    console.log('---target---', target);
+    return target;
+  })
+}
+
+findTargetStringFromSource('target')
