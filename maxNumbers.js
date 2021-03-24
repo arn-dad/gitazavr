a0bc84218e08f13612c61db1644bba81b16b5064
ArnoldDadayan <dadayanaa@yahoo.com>
2021-03-24 21:13:47 +0400

    Update properties for function

--- maxNumbers.js
+++ maxNumbers.js
@@ -2,6 +2,6 @@ const findMaxNumbers = (numbers, length) => {
   // ...
 }
 
-const output = findMaxNumbers([5,6,8,9,15], 3)
+const output = findMaxNumbers([5,16,8,9,15, 20], 3)
 
 console.log('output: ', output);
